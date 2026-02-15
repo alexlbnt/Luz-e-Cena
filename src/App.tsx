ac21c30ee40b6364b16e6c989de2040727bed8c5
Alexandre Lopes <alexbarreiraneto@hotmail.com>
2026-02-15 16:29:14 -0300

    Refactor App component and add Banner and Link components with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import './App.css'
+import Banner from './components/Banner'
+import Link from './components/Link'
 
 function App() {
-  return <h1>Porra</h1>;
+  return (
+    <>
+      <Banner src="./public/Imagens/Banner principal/Banner principal desktop.png" alt="Banner" />
+      <Link href="/" target="_blank">
+        Link
+      </Link>
+    </>
+  )
 }
 
 export default App
